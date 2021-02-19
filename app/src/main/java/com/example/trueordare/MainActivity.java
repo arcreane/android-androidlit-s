190b9549484c68698f1b49ff2c3151378694ca3a
ABY-sys <abygueye2@gmail.com>
2021-02-19 16:53:20 +0100

    Suite

--- app/src/main/java/com/example/trueordare/MainActivity.java
+++ app/src/main/java/com/example/trueordare/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trueordare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,23 +10,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn_cnx;
-    TextView tv_ins;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*btn_cnx = (Button) findViewById(R.id.button2);
-        tv_ins = (TextView) findViewById(R.id.editTextTextPersonName2);
-        btn_cnx.setOnClickListener(this);
-        tv_ins.setOnClickListener(this);*/
-
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+     switch (v.getId()) {
+         case R.id.button2:
+             Intent i = new Intent(this, pageJeu.class);
+             startActivity(i);
+             break;
+     }
     }
 }
