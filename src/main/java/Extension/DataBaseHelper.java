94128fc353b3380aeb3cf3e1ee5d35f6adf7d1a7
MorganMBA <mba@intechinfo.fr>
2021-02-19 21:55:50 +0100

    update TrueOrDare.db

--- src/main/java/Extension/DataBaseHelper.java
+++ src/main/java/Extension/DataBaseHelper.java
@@ -16,6 +16,12 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String PARTY = "Party";
     public static final String PARTY_DATE = "PARTY_DATE";
     public static final String PARTY_ID = "PARTY_ID";
+    public static final String PLAYER = "PLAYER";
+    public static final String PLAYER_ID = "PLAYER_ID";
+    public static final String PLAYER_Name = "PLAYER_Name";
+    public static final String QUESTION = "QUESTION";
+    public static final String QUESTION_ID = "QUESTION_ID";
+    public static final String QUESTION_Entitled = "QUESTION_Entitled";
 
     public DataBaseHelper(@Nullable Context context) {
         super(context, "TrueOrDare.db", null, 1);
@@ -23,7 +29,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE " + PARTY + " (" + PARTY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PARTY_DATE + " Date)";
+        StringBuilder createTableStatement = new StringBuilder("CREATE TABLE " + PARTY + " (" + PARTY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PARTY_DATE + " Date)");
+        createTableStatement.append("CREATE TABLE " + PLAYER + " (" + PLAYER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + PLAYER_Name + " Varchar)");
+        createTableStatement.append("CREATE TABLE " + QUESTION + " (" + QUESTION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + QUESTION_Entitled + " Varchar)");
     }
 
     @Override
