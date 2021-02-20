731f46319b7f978c5e63adf3a9b4c760f181a8dd
Sira Mendy <siramendy6@gmail.com>
2021-02-20 21:19:42 +0100

    'final'

--- app/src/main/java/com/example/tord/Menu.java
+++ app/src/main/java/com/example/tord/Menu.java
@@ -1,48 +1,37 @@
-/*package com.example.tord;
+
+
+/*PROBLEME AVEC CETTE PAGE LE CODE BUG*/
+
+
+/*package com.example.tord.;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.os.Bundle;
 
 public class Menu extends AppCompatActivity {
-    Button button3;
-    Button button5;
-
-        @Override
-        protected void  onCreate( Bundle savedInstanceState ) {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        button = (Button) findViewById(R.id.button);
 
+    }
 
-        button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(v ->{
-        Intent i = new Intent(this, AjoutJoueur.class);
-        startActivity(i);
-        });}
+    /*public void diminuer(View view) {
+        EditText number = (EditText) findViewById(R.id.editTextNumber);
+        number.setText("editTextNumber" - 1 );
+    }*/
 
-
-    public void augmenter( ) {
-
-
-
-        var Contenu = textView1.text.toString().toInt();
-        Contenu = Contenu + 1;
-        textView1.text = Contenu.toString;
-        }
-
-    protected void diminuer() {
-        var Contenu = textView1.text.toString().toInt();
-        if (Contenu > 0) {
-        Contenu = Contenu - 1;
-        }
-        textView1.text = Contenu.toString;
-        }
-        }
-
-
+   /* public void augementer(View view) {
+        EditText number = (EditText) findViewById(R.id.editTextNumber);
+        number.setText("editTextNumber" + 1 );
+    }
+}
 */
-
